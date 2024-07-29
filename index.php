f76f7bcab6e9f40493894d19682b5ab1ffec85c7
IgorMaia055 <igormaia055@gmail.com>
2024-07-28 22:04:46 -0300

    Versão oficial 1.0

--- index.php
+++ index.php
@@ -16,18 +16,16 @@ $zamplate->addFunction('soma', [Helpers::class, 'soma']);
 
 // Renderiza o template com dados
 echo $zamplate->render('example.html', [
-    'name' => 'World',
+    'text' => 'World',
     'numero' => 23,
     'bool' => true,
     'array' => [[
             'id_colaborador' => 345,
-            'teste',
             'status' => true,
             'codigo' => 12343
         ],
         [
             'id_colaborador' => 456,
-            'teste',
             'status' => false,
             'codigo' => 67890
         ]]
